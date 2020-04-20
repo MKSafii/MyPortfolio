6fe32d9ff1c2b2f9ac15038c071a4983ff2cb4de
MKSafii <engmksafi@gmail.com>
2020-04-20 03:54:53 +0300

    add view and controllers

--- web/Startup.cs
+++ web/Startup.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.Interfaces;
 using Infrastructure;
+using Infrastructure.UnitOfWork;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -30,7 +32,7 @@ namespace web
                 options.UseSqlServer(configuration.GetConnectionString("MyPortfolioDB"));
             });
 
-          //  services.AddScoped(typeof(IUnitOfWork<>), typeof(UnitOfWork<>));
+            services.AddScoped(typeof(IUnitOfWork<>), typeof(UnitOfWork<>));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
